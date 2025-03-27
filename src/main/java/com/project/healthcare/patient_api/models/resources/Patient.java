786b8bdf208e3bbf5cf745ac7ee4712b72eb3ab4
Brandon Zhang <zhangtbrandon2@gmail.com>
2025-03-27 16:34:14 -0400

    Created Patient Builder

--- src/main/java/com/project/healthcare/patient_api/models/resources/Patient.java
+++ src/main/java/com/project/healthcare/patient_api/models/resources/Patient.java
@@ -7,32 +7,65 @@ import com.project.healthcare.patient_api.models.elements.*;
 import com.project.healthcare.patient_api.models.elements.backbone.*;
 import com.project.healthcare.patient_api.models.elements.codes.*;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
 @Data
+@Builder
 @EqualsAndHashCode(callSuper = true)
 public class Patient extends Resource {
 
-  private boolean active; // Whether the record is in active use
+  @Builder.Default
+  private boolean active = true; // Whether the record is in active use
+
+  @NonNull
   private HumanName name;
+
+  @NonNull
   private Date birthDate;
-  private boolean deceased;
+
+  @NonNull
   private Gender gender;
+
+  @Builder.Default
+  private boolean deceased = false;
+
   private Date deceasedDate;
+
   private Address address;
+
+  @NonNull
   private MaritialStatus maritalStatus;
+
   private boolean multipleBirth;
+
   private int multipleBirthInteger;
+
   private Blob photo;
+
+  @NonNull
   private Contact contact;
+
+  @NonNull
   private ContactPoint telecom;
+
   private Communication communication;
   // private Reference generalPractitioner;
   // private Reference managingOrganization;
 
-  public Patient() {
-    super();
+  public static PatientBuilder build() {
+    return new CustomPatientBuilder();
+  }
+
+  public static class CustomPatientBuilder extends PatientBuilder {
+    @Override
+    public Patient build() {
+      // Additional checks here
+
+      return super.build();
+    }
   }
 
 }
